16993b3b0eaa02da66e8f346c825b9236ad40e14
Cybergneeda <a.shamyanov@mail.ru>
2024-03-04 12:02:26 +0300

    Исправил ошибки

--- Part_2.java
+++ Part_2.java
@@ -13,7 +13,7 @@ public static void main(String[] args)
     System.out.println("Строка начинается со слова \"волк\": "+str1.startsWith("волк"));
     System.out.println("Длина строки: "+str1.length()+" символа"); 
     System.out.println("Строка содержит слово \"волк\": "+str1.contains("волк"));
-    System.out.println("Строка содержит слово \"лев\": "+str1.startsWith("лев"));
+    System.out.println("Строка содержит слово \"лев\": "+str1.contains("лев"));
     System.out.println("Строка пуста: "+str1.isEmpty());
     String str2=str1.substring(0,17);
     System.out.println("Первая часть строки: "+str2);
